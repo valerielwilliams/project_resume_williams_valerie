da8abc25d0503121914cc0215b56cd0a3ff924ad
Valerie Williams <valerielwilliams@ufl.edu>
2017-04-07 13:54:31 -0400

    add Gradientify jQuery plugin
    I wanted my background to have changing gradients, and I found a cool
    jQuery plugin to do just that. I added the jquery.gradientify.min.js
    file to the js folder, referenced it in the HTML and added the code to
    the main.js file. Neat. Now to change the RGB values!

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('body').gradientify({
+        gradients: [
+            { start: [49, 76, 172], stop: [242, 159, 191] },
+            { start: [255, 103, 69], stop: [240, 154, 241] },
+            { start: [33, 229, 241], stop: [235, 236, 117] }
+        ]
+    });
+
+});
